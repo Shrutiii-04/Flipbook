baa50e0ff50fcb939e7a7634fc430a585704921e
Shrutiii-04 <shrutipatil0880@gmail.com>
2025-09-17 16:53:25 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('startButton').addEventListener('click', function() {
+    // Redirect to the other webpage when the button is clicked
+    window.location.href = 'C:\Users\Family Users\Desktop\Shruti Patil\Flipook\Final Code\Page 1.html';
+});
